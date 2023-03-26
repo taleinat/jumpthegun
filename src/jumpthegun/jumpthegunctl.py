8d8db65ab1699fe5f3f7bbcd9fe819b08690efa6
Tal Einat <532281+taleinat@users.noreply.github.com>
2023-03-26 22:17:19 +0300

    Add daemon idle timeout

--- src/jumpthegun/jumpthegunctl.py
+++ src/jumpthegun/jumpthegunctl.py
@@ -1,5 +1,6 @@
 import hashlib
 import io
+import json
 import os
 import random
 import shlex
@@ -11,6 +12,7 @@ import sys
 import tempfile
 import time
 import traceback
+from dataclasses import dataclass
 from pathlib import Path
 from typing import Any, BinaryIO, Optional, Set, Tuple, Union, cast
 
@@ -106,6 +108,40 @@ class StdinWrapper(io.RawIOBase):
         return self._sock.fileno()
 
 
+def get_xdg_config_dir() -> Path:
+    env_var = os.environ.get('XDG_CONFIG_HOME')
+    if env_var:
+        return Path(env_var)
+    return Path.home() / '.config'
+
+
+@dataclass(frozen=True)
+class JumpTheGunConfig:
+    idle_timeout_seconds: Optional[int] = 4 * 60 * 60  # 4 hours
+
+    def __post_init__(self):
+        if self.idle_timeout_seconds is None:
+            pass
+        elif isinstance(self.idle_timeout_seconds, int):
+            if self.idle_timeout_seconds <= 0:
+                raise ValueError("idle_timeout_seconds must be positive.")
+        else:
+            raise TypeError("idle_timeout_seconds must be an int or None.")
+
+
+def read_config() -> JumpTheGunConfig:
+    config_dir = get_xdg_config_dir()
+    if not config_dir.exists():
+        return JumpTheGunConfig()
+    config_file = config_dir / "jumpthegun.json"
+    if not config_file.exists():
+        return JumpTheGunConfig()
+    with config_file.open(encoding="utf-8") as f:
+        config_data = json.load(f)
+    config = JumpTheGunConfig(**config_data)
+    return config
+
+
 def get_service_runtime_dir_path() -> Path:
     runtime_dir = os.getenv("XDG_RUNTIME_DIR")
     if runtime_dir:
@@ -184,6 +220,8 @@ def daemon_teardown(
 
 
 def start(tool_name: str, daemonize: bool = True) -> None:
+    config = read_config()
+
     tool_runner = get_tool_runner(tool_name)
 
     pid_file_path, port_file_path = get_pid_and_port_file_paths(tool_name)
@@ -235,6 +273,7 @@ def start(tool_name: str, daemonize: bool = True) -> None:
     # Listen for connections.
     sock.listen()
     print(f"Listening on {host}:{port} (pid={pid}) ...")
+    sock.settimeout(config.idle_timeout_seconds)
     subproc_pids = set()
     try:
         while True:
@@ -248,7 +287,7 @@ def start(tool_name: str, daemonize: bool = True) -> None:
             done_subproc_pids = {x for x in subproc_pids if os.waitpid(x, os.WNOHANG)[0] != 0}
             subproc_pids -= done_subproc_pids
             subproc_pids.add(newpid)
-    except BaseException:
+    except BaseException as exc:
         # Server is exiting: Clean up as needed.
         sock.close()
         if pid_file_path.exists():
@@ -256,6 +295,9 @@ def start(tool_name: str, daemonize: bool = True) -> None:
             if file_pid == pid:
                 pid_file_path.unlink(missing_ok=True)
                 port_file_path.unlink(missing_ok=True)
+        if isinstance(exc, socket.timeout):
+            print(f"Exiting after receiving no connections for {config.idle_timeout_seconds} seconds.")
+            return
         raise
 
     # Send pid.
